fd925a7bd08597b2d2db70f04c82b0e23a262a3f
ethic41 <dahirmuhammad3@gmail.com>
2020-03-19 20:42:38 +0100

    generate python ui files

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,9 +1,9 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
-from core.amazon_scraper.scraper import Scraper
+from ui.keetext import KeetextGui
+
 import sys
 
-
+"""
 def test():
     my_scraper = Scraper()
     categories_soup = my_scraper.make_categories_soup()
@@ -12,13 +12,11 @@ def test():
     for items in subcategories:
         for item in subcategories[items]:
             print(subcategories[items][item])
-
+"""
 
 if __name__ == '__main__':
-    test()
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
+    window = KeetextGui()
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
---
+++ src/main/python/ui/keetext.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-03-19 13:09:17
+# @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+
+from ui.main_window import Ui_MainWindow
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import os
+
+
+class KeetextGui(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(KeetextGui, self).__init__()
+        self.setupUi(self)
+        
