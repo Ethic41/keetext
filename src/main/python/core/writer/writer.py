c6c83918eeb0fb0cff53d6b090030a9d2f4a009a
ethic41 <dahirmuhammad3@gmail.com>
2020-03-24 16:36:23 +0100

    created writer module

---
+++ src/main/python/core/writer/writer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-03-24 15:37:25
+# @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+
+import openpyxl
+
+
+class Writer:
+
+    def __init__(self):
+        pass
+
+    def write_to_file(self, output_file_format, output_dir, products):
+        pass
