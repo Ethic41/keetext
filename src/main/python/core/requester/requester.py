2406501d5390abc49d9580e03f6de73a6e73309f
ethic41 <dahirmuhammad3@gmail.com>
2020-03-26 18:56:11 +0100

    fixed spelling of requester

---
+++ src/main/python/core/requester/requester.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020-03-18 01:51:48
+# @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+
+from core.requester.get_user_agent import get_user_agent
+import requests
+
+# TODO: we should implement get user-agents from internet
+# with in this module
+class Requester:
+    def __init__(self):
+        self.user_agent = get_user_agent()
+        self.headers = {"user-agent": self.user_agent}
+        with requests.Session() as conn:
+            self.conn = conn
+    
+    def make_request(self, url: str, request_method="get", payload: dict=None):
+        if request_method == "get":
+            request = self.conn.get(url, params=payload, headers=self.headers, timeout=(9, 27))
+            return request.content
+        
+        if request_method == "post":
+            request = self.conn.post(url, data=payload, headers=self.headers, timeout=(9, 27))
+            return request.status_code
